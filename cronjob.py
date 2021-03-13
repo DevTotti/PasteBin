b88bbaf180281b5bef7f06ab2d2d0d00a5fa890b
Paul <devtotti@gmail.com>
2021-03-13 14:44:52 +0100

    adding data

--- cronjob.py
+++ cronjob.py
@@ -1,3 +1,17 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
+from expiry import deleteDocument
 
+scheduler = BlockingScheduler()
+# scheduler.add_job(deleteDocument, "interval", seconds=10, max_instances=2)
 
+scheduler.add_job(
+    func=deleteDocument,
+    trigger=IntervalTrigger(seconds=2),
+    id='deleting my data',
+    replace_existing=True
+)
+scheduler.start()
+
+# atexit.register(lambda: scheduler.shutdown())
